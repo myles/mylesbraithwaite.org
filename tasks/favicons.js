b4a260b31d1b3ba17117f5e2186730855bedcc9c
Myles Braithwaite <me@mylesbraithwaite.com>
2016-03-17 13:57:21 -0400

    Fixing some lint issues.

--- tasks/favicons.js
+++ tasks/favicons.js
@@ -3,19 +3,19 @@ module.exports = function (grunt) {
 
     var config = {
         options: {
-          trueColor: true,
-          precomposed: true,
-          appleTouchBackgroundColor: "#f7f7f7",
-          coast: true,
-          windowsTile: true,
-          tileBlackWhite: false,
-          tileColor: "#f7f7f7",
-          appleTouchPadding: 20,
-          firefox: true,
-          androidHomescree: true,
-          html: 'source/_includes/head_favicons.html',
-          HTMLPrefix: '/',
-          indent: ''
+            trueColor: true,
+            precomposed: true,
+            appleTouchBackgroundColor: "#f7f7f7",
+            coast: true,
+            windowsTile: true,
+            tileBlackWhite: false,
+            tileColor: "#f7f7f7",
+            appleTouchPadding: 20,
+            firefox: true,
+            androidHomescree: true,
+            html: 'source/_includes/head_favicons.html',
+            HTMLPrefix: '/',
+            indent: ''
         },
         icons: {
             src: './source/_assets/images/beaker.500x500.png',
